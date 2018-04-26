a56e35f4d63b80e3a48aa4c8c951584a48a1a937
ghr <ghrsevn@live.com>
2018-04-26 21:44:30 +0800

    night

--- n1-admin/src/store/index.js
+++ n1-admin/src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import {login} from './modules/login'
 import {home} from './modules/home'
-import {report} from './modules/ugreport'
+import {report} from './modules/report'
 Vue.use(Vuex)
 export const store = new Vuex.Store({
     modules: {
