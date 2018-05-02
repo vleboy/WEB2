cffbe043f2af459675bf701d6b08080577ca2e0d
hongrong <ghrace@live.com>
2018-05-02 15:38:32 +0800

    url

--- n1-admin/src/service/urlConfig.js
+++ n1-admin/src/service/urlConfig.js
@@ -1,9 +1,11 @@
 console.log(process.env.NODE_ENV);
+export const URL='d3rqtlfdd4m9wd.cloudfront.net'
+// export let URL = (process.env.NODE_ENV == 'development') 
+// ? 'd3rqtlfdd4m9wd.cloudfront.net' : 'n1admin.na12345.com';
+// console.log(URL);
 
-export let URL = (process.env.NODE_ENV == 'development') 
-? 'd3rqtlfdd4m9wd.cloudfront.net' : 'n1admin.na12345.com';
 export const httpType = 'https://'
-console.log(URL);
+
 export const api = {
   getVaptcha: httpType + URL + '/vaptcha/getVaptcha',
   getDownTime: httpType + URL + '/vaptcha/getDownTime'
