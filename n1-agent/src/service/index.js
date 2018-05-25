64f56ad24eece3da0f56018ff1e5f7959ea13423
gp <gp646890523@sina.com>
2018-05-25 17:18:52 +0800

    添加一些配置

--- n1-agent/src/service/index.js
+++ n1-agent/src/service/index.js
@@ -2,55 +2,73 @@ import { http } from './http'
 import { URL,httpType } from './urlConfig'
 // import QS from 'qs'
 
-// get请求
-const get = urls =>{
-    let token=localStorage.getItem('n1token');
-    let headers={
-        "Content-Type": "application/json; charset=utf-8",
-        "Authorization":token
-    }
-    return {
-     method: 'get',
-     url: httpType+ URL + urls,
-     headers: headers
-    }
+// get请求 (url拼接参数，请求域名类别）
+// 域名类别为: 不传域名默认为n1, 游戏相关接口为game
+const get = (urls, type) => {
+  let token=localStorage.getItem('n1token');
+  let headers={
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization":token
+  }
+  return {
+    method: 'get',
+    url: httpType+ URL(type) + urls,
+    headers: headers
+  }
 }
+
 // post请求
-const post = (urls, datas) => {
-    let TOKEN='';
-    let headers={};
-    if(window.localStorage.getItem('n1token')) {
-        TOKEN = window.localStorage.getItem('n1token')
-        headers={
-            "Content-Type": "application/json; charset=utf-8",
-            "Authorization":TOKEN
-        }
-    }else{
-        headers={
-            "Content-Type": "application/json; charset=utf-8",
-        }
+const post = (urls, datas, type) => {
+  let TOKEN='';
+  let headers={};
+  if(window.localStorage.getItem('n1token')) {
+    TOKEN = window.localStorage.getItem('n1token')
+    headers={
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization":TOKEN
     }
-    return {
-        method: 'post',
-        url: httpType+ URL + urls,
-        data:datas,                       //QS.stringify(datas),
-        headers: headers
+  }else{
+    headers={
+      "Content-Type": "application/json; charset=utf-8",
     }
-}
-//img
-const imgpost = (urls, datas) => ({
+  }
+  return {
     method: 'post',
-    url: URL + urls,
-    data: datas,
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        "Content-Type": "multipart/form-data"
-    }
+    url: httpType+ URL(type) + urls,
+    data:datas,                       //QS.stringify(datas),
+    headers: headers
+  }
+}
+
+//img
+const Image = (method, url, data) => ({
+  method: method,
+  url: url,
+  data: data,
+  headers: {
+    "Content-Type": "image"
+  }
 })
 
 //example
 export async function example(params) {
-    return http()
+  return http()
+}
+
+//针对业务逻辑里的请求
+export async function httpRequest(method,url,params,type) {
+  //参数分别作用 （方法, 请求接口, 参数, 域名接口）
+  switch (method) {
+    case 'get':
+      return http(get(url,type))
+      break
+    case 'post':
+      return http(post(url,params,type))
+      break
+    case 'put':
+      return http(Image(method,url,params))
+      break
+  }
 }
 //登录
 export async function logIn(role,username,password,challenge,vid,cb,err){
--- n1-agent/src/service/urlConfig.js
+++ n1-agent/src/service/urlConfig.js
@@ -1,9 +1,31 @@
+// console.log(process.env.NODE_ENV);
+// export const URL='d3rqtlfdd4m9wd.cloudfront.net'
+export let URL = type => {
+  if (process.env.NODE_ENV == 'production') {
+    switch (type) {
+      case 'game':
+        return 'n1game.na12345.com'
+        break
+      default:
+        return 'n1admin.na12345.com'
+        break
+    }
+  } else {
+    switch (type) {
+      case 'game':
+        return 'dgjmusja39cm2.cloudfront.net'
+        break
+      default:
+        return 'd3prd6rbitzqm3.cloudfront.net'
+        break
+    }
+  }
+}
+// console.log(URL);
 
-export let URL = (process.env.NODE_ENV == 'development') 
-? 'd3prd6rbitzqm3.cloudfront.net' : '';
 export const httpType = 'https://'
 
 export const api = {
-  getVaptcha: httpType + URL + '/vaptcha/getVaptcha',
-  getDownTime: httpType + URL + '/vaptcha/getDownTime'
+  getVaptcha: httpType + URL() + '/vaptcha/getVaptcha',
+  getDownTime: httpType + URL() + '/vaptcha/getDownTime'
 }
