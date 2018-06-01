fd8dc22e60374db0361e853f4c91d1e2d7e925e7
gp <gp646890523@sina.com>
2018-06-01 11:11:55 +0800

    增加看板

--- n1-agent/src/components/sidebar.vue
+++ n1-agent/src/components/sidebar.vue
@@ -3,10 +3,9 @@
         <Sider width='256px' collapsible hide-trigger :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
             <img class="logoimg" src="../../static/logo.png" alt="">
             <Menu ref="sideMenu" :active-name="$route.name" :open-names="openName" theme="dark" width="auto" @on-select='selectMenu'>
-                <!-- <MenuItem name="home">
-                <span>看板</span>
+                <MenuItem name="board">
+                  <span>看板</span>
                 </MenuItem>
-                -->
                 <MenuItem name="ownspace-index">
                 <span>个人中心</span>
                 </MenuItem>
