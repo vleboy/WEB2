4643eca42d9f9c39bcd5a78c4bc6206eee784d7f
ghr <ghrsevn@live.com>
2018-06-01 21:57:44 +0800

    router

--- n2-agent/src/router/router.js
+++ n2-agent/src/router/router.js
@@ -1,5 +1,26 @@
 import main from '@/pages/main'
+import login from '@/pages/login'
+import err404 from '@/pages/404'
+import home from '@/pages/home'
+import playerDetail from '@/pages/player/playerdetail'
+import agentDetail from '@/pages/agentCenter/agentDetail'
+//report
+import ownSpace from '@/pages/ownSpace'
+// import board from '@/pages/board/board'
+import nacard from '@/pages/reports/naCardGame'
+import nastreet from '@/pages/reports/naStreetGame'
+import natrue from '@/pages/reports/naTrueGame'
+import navideo from '@/pages/reports/naVideoGame'
+import naAll from '@/pages/reports/naAllReport'
 
+import agentList from '@/pages/agentCenter/agentList'
+import warnList from '@/pages/agentCenter/warnList'
+import adminLoginLog from '@/pages/logCenter/adminLoginLog'
+import agentLoginLog from '@/pages/logCenter/agentLoginLog'
+import adminOpreateLog from '@/pages/logCenter/adminOpreateLog'
+import adminList from '@/pages/adminCenter/adminList'
+import gameConfig from '@/pages/gameCenter/gameConfig'
+import playerList from '@/pages/player/playerlist'
 export const loginRouter = {
     path: '/login',
     name: 'login',
@@ -9,7 +30,7 @@ export const loginRouter = {
     meta: {
         requireAuth: false
       },
-    component: () => import('@/pages/login.vue')
+    component: login
 };
 export const page404 = {
     path: '*',
@@ -18,7 +39,7 @@ export const page404 = {
         title: '404-页面不存在',
         requireAuth: false
     },
-    component: () => import('@/pages/404.vue')
+    component: err404
 };
 // 作为main组件的子页面展示但是不在左侧菜单显示的路由写在otherRouter里
 export const otherRouter = {
@@ -28,9 +49,9 @@ export const otherRouter = {
     title:'other',
     component: main,
     children: [
-        { path: 'home', name: 'home',title:'首页', component: () => import('@/pages/home.vue')},
-        { path: 'agentDetail', name: 'agentDetail',title:'代理详情', component: () => import('@/pages/agentCenter/agentDetail.vue')},
-        { path: 'detail', name:'playDetail', title:'玩家详情', component:()=>import('@/pages/player/playerdetail.vue')}
+        { path: 'home', name: 'home',title:'首页', component: home},
+        { path: 'agentDetail', name: 'agentDetail',title:'代理详情', component: agentDetail},
+        { path: 'detail', name:'playDetail', title:'玩家详情', component:playerDetail}
     ]
 };
 // 作为main组件的子页面展示并且在左侧菜单显示的路由写在appRouter里
@@ -40,7 +61,7 @@ export const appRouter = [
         title:'个人中心',
         name:'ownspace',
         component:main,
-        children:[{path:'index',title:'个人中心', name:'ownspace-index',component:()=>import('@/pages/ownSpace.vue')}]
+        children:[{path:'index',title:'个人中心', name:'ownspace-index',component:ownSpace}]
     },
     {
         path:'/nagame',
@@ -48,11 +69,11 @@ export const appRouter = [
         name:'nagame',
         component:main,
         children:[
-            {path:'card',title:'NA棋牌游戏报表',name:'nacard',component:()=>import('@/pages/reports/naCardGame.vue')},
-            {path:'street',title:'NA街机游戏报表',name:'nastreet',component:()=>import('@/pages/reports/naStreetGame.vue')},
-            {path:'true',title:'NA真人游戏报表',name:'natrue',component:()=>import('@/pages/reports/naTrueGame.vue')},
-            {path:'video',title:'NA电子游戏报表',name:'navideo',component:()=>import('@/pages/reports/naVideoGame.vue')},
-            {path:'all',title:'NA游戏总报表',name:'naAll',component:()=>import('@/pages/reports/naAllReport.vue')}
+            {path:'card',title:'NA棋牌游戏报表',name:'nacard',component:nacard},
+            {path:'street',title:'NA街机游戏报表',name:'nastreet',component:nastreet},
+            {path:'true',title:'NA真人游戏报表',name:'natrue',component:natrue},
+            {path:'video',title:'NA电子游戏报表',name:'navideo',component:navideo},
+            {path:'all',title:'NA游戏总报表',name:'naAll',component:naAll}
         ]
     },
     {
@@ -61,8 +82,8 @@ export const appRouter = [
         name:'agent',
         component:main,
         children:[
-            {path:'list',title:'代理列表',name:'agentList',component:()=>import('@/pages/agentCenter/agentList.vue')},
-            {path:'warn',title:'警告列表',name:'warnList',component:()=>import('@/pages/agentCenter/warnList.vue')}
+            {path:'list',title:'代理列表',name:'agentList',component:agentDetail},
+            {path:'warn',title:'警告列表',name:'warnList',component:warnList}
         ]
     },
     {
@@ -71,9 +92,9 @@ export const appRouter = [
         name:'log',
         component:main,
         children:[
-            {path:'adminLogin',title:'管理员登录日志',name:'adminLoginLog',component:()=>import('@/pages/logCenter/adminLoginLog.vue')},
-            {path:'agentLogin',title:'代理登录日志',name:'agentLoginLog',component:()=>import('@/pages/logCenter/agentLoginLog.vue')},
-            {path:'adminOpreate',title:'管理员操作日志',name:'adminOpreateLog',component:()=>import('@/pages/logCenter/adminOpreateLog.vue')},
+            {path:'adminLogin',title:'管理员登录日志',name:'adminLoginLog',component:adminLoginLog},
+            {path:'agentLogin',title:'代理登录日志',name:'agentLoginLog',component:agentLoginLog},
+            {path:'adminOpreate',title:'管理员操作日志',name:'adminOpreateLog',component:adminOpreateLog},
         ]
     },
     {
@@ -82,7 +103,7 @@ export const appRouter = [
         name:'admin',
         component:main,
         children:[
-            {path:'list',title:'管理员列表',name:'adminList',component:()=>import('@/pages/adminCenter/adminList.vue')},
+            {path:'list',title:'管理员列表',name:'adminList',component:adminList},
         ]
     },
     {
@@ -91,7 +112,7 @@ export const appRouter = [
         name:'game',
         component:main,
         children:[
-            {path:'config',title:'包房代理游戏配置',name:'gameConfig',component:()=>import('@/pages/gameCenter/gameConfig.vue')},
+            {path:'config',title:'包房代理游戏配置',name:'gameConfig',component:gameConfig},
         ]
     },
     {
@@ -100,7 +121,7 @@ export const appRouter = [
       name:'play',
       component:main,
       children:[
-        {path:'list',title:'玩家列表',name:'playList',component:()=>import('@/pages/player/playerlist.vue')}
+        {path:'list',title:'玩家列表',name:'playList',component:playerList}
       ]
     }
 
