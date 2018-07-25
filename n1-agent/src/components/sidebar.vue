1265797ddcc14db596667c1f0f26a3c9808ce0ba
hongrong <ghrace@live.com>
2018-07-25 09:37:21 +0800

    board

--- n1-agent/src/components/sidebar.vue
+++ n1-agent/src/components/sidebar.vue
@@ -3,7 +3,7 @@
         <Sider width='256px' collapsible hide-trigger :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
             <img class="logoimg" src="../../static/logo.png" alt="">
             <Menu ref="sideMenu" :active-name="$route.name" :open-names="openName" theme="dark" width="auto" @on-select='selectMenu'>
-                <MenuItem name="board" v-if='permission.includes("看板")'>
+                <MenuItem name="board" v-if='permission.includes("看板") || level!=0'>
                     <Icon type="stats-bars"></Icon>
                     <span>看板</span>
                 </MenuItem>
