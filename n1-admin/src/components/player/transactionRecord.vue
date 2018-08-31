9199743489c44e5a43c8651f4f08340f2859f700
hongrong <ghrace@live.com>
2018-08-31 10:24:28 +0800

    balance

--- n1-admin/src/components/player/transactionRecord.vue
+++ n1-admin/src/components/player/transactionRecord.vue
@@ -30,8 +30,8 @@
           <Button type="primary" @click="exportData">导出数据</Button>
         </Col>
         <Col span="7">
-          <span class="justfy2">当前剩余点数：<span style="color: #F7BA2A">{{formatPoints(balance)}}</span></span>
-          <Button type="text" @click="resultGetPlayerDetail">刷新</Button>
+          <!-- <span class="justfy2">当前剩余点数：<span style="color: #F7BA2A">{{formatPoints(balance)}}</span></span> -->
+          <Button @click="resultGetPlayerDetail">刷新</Button>
         </Col>
       </Row>
     </div>
@@ -108,7 +108,7 @@
         playerDetailList: [],
         playerDetailListStorage: [],
         playerDetailStartKey: '',
-        balance: '',
+        // balance: '',
         betId: '',
         propChild: {},
         runningDetail: {},
@@ -320,7 +320,7 @@
           result => {
             this.isLastMessage = result.list < this.pageSize
             this.playerDetailList = result.list
-            this.balance = result.balance
+            // this.balance = result.balance
             this.playerDetailStartKey = result.startKey
             this.playerDetailListStorage.length && (this.playerDetailList = this.playerDetailListStorage.concat(this.playerDetailList))
             this.isFetching = false
